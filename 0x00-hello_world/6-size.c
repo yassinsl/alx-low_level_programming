f8c8ea18d2cd195dab4cea3d73dfa4fdd3d05939
YASSINE <slawiyassin152@gmail.com>
2023-09-07 14:38:44 -0700

    this is C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: Prints a message to the console.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of a char :%lu byte\n",sizeof(char);
+printf("Size of a int :%lu bytes\n",sizeof(int);
+printf("Size of a long int :%lu byte\n",sizeof(long int);
+printf("Size of a long long :%lu byte\n",sizeof(long long);
+printf("Size of a fleat :%lu byte\n",sizeof(float);
+return (0);
+}
