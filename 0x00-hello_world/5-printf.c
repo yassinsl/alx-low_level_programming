85028df3df94924a2f87cb53e3570ed8df687805
YASSINE <slawiyassin152@gmail.com>
2023-09-07 14:15:33 -0700

    this is C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
 *Description: Prints a message to the console.
 *Return: Always 0 (Success)
  */
-int main (void){
+int main(void){
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
