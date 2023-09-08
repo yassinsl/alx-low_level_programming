52958822136016a94f6dbb176bfa5eded315c785
YASSINE <slawiyassin152@gmail.com>
2023-09-08 14:38:48 -0700

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -enty point
+ * Description:print all alpha letteres
+ * REturn:Always 0 (Success)
+*/
+
+int main(void)
+{
+ char a = 'a';
+while (a<='z'){
+putchar(a);
+a++;
+}
+putchar('\n');
+}
