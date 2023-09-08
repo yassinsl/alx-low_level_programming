6524b923ed795b1d551bc1f25fb35af9161c9dbd
YASSINE <slawiyassin152@gmail.com>
2023-09-08 16:06:42 -0700

    prints all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main -entry point
+*
+* Description :prints all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int num = 0;
+while (a <= 9)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
