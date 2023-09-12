5612412f6af089722745d3c930324e6f05158faf
YASSINE <slawiyassin152@gmail.com>
2023-09-12 10:43:24 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet 0utilizes on the _putchar function to print the alphapit a - z
+*/
+void print_alphabet(void)
+
+{
+	char = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a + 'a');
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
