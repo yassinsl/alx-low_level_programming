af8a8cec76bee4d073068d4ca617a6b8fe0019ce
YASSINE <slawiyassin152@gmail.com>
2023-10-06 07:30:10 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,34 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * str_concat - function that concatenates two strings
- *
- * @s1: string of chars
- * @s2: string of chars
- *
- * Return: address of the newly allocated memory
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
  */
-
-char *str_concat(char *s1, char *s2)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i = 0, j = 0;
-	int len1 = 0, len2 = 0;
-	char *result;
+	void *ptr;
 
-	if (s1 != NULL)
-		len1 = strlen(s1);
-	if (s2 != NULL)
-		len2 = strlen(s2);
-
-	result = malloc(len1 + len2 + 1);
-	if (result == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		result[i] = s1[i];
-	for (j = 0; j < len2; j++)
-		result[i + j] = s2[j];
-	result[i + j] = '\0';
-	return (result);
+	ptr = malloc(nmemb * size);
+		if (ptr == NULL)
+			return (NULL);
+	return (ptr);
 }
