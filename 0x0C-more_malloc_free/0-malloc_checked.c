f0c8b2c39b15d76d9c88a401cdeab72573945d6b
YASSINE <slawiyassin152@gmail.com>
2023-10-06 02:59:13 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * malloc_checked - Allocates memory using malloc and checks for success.
+ *
+ * @b: The number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ *         If malloc fails, it exits the process with status 98.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *yassin;
+
+	yassin = malloc(b);
+	if (yassin == NULL)
+	{
+		exit(98)
+	}
+	return (yassin);
+}
