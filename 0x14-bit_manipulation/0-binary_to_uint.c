5d8f12cddcd768cd0d74e4ec52fc77db7331b19b
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2023-12-14 16:44:44 +0100

    up_date

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, decemale, rem; 
+	unsigned int i = 0, decemale = 0, rem = 0; 
 
-	if (b[0] == '\0')
+if (b[0] == '\0')
 	return (0);
 
 while (b[i])
 	{
-	if (b[i] != '0' && b[i] != '1')
-		return (0);
+if (b[i] != '0' && b[i] != '1')
+return (0);
 		else
 		{	
 			rem = b[i] - '0';
