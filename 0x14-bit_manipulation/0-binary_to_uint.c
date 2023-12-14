0e4a8c375ea56899a08434a4dddd0cb7b366aa5b
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2023-12-14 16:20:37 +0100

    UPDATE

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0, decemale = 0,rem = 0;
+
+	if (b[0] == '\0')
+	return 0;
+
+while (b[i])
+	{
+	if (b[i] != '0' && b[i] != '1')
+		return 0;
+		else
+		{	
+			rem = b[i] - '0';
+			decemale = decemale * 2 + rem;
+		}
+			i++;	
+		}
+	return (decemale);
+	}
