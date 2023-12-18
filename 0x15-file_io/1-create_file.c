9a53326e98b5d8889f2839904ab89e8db89b7858
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2023-12-18 14:27:39 +0100

    alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * create_file - creates a file
  * @filename: filename.
@@ -6,36 +7,32 @@
  *
  * Return: 1 if it success. -1 if it fails.
  */
-
-int _strlen(char * cont )
-{
-	int i = 0 ;
-
-	if(cont == NULL)
-	return (0);
-	while(cont[i])
-	i++;
-	return (i);
-}
-
 int create_file(const char *filename, char *text_content)
 {
-	int *fp = open(filename,O_WRONLY | O_CREAT |O_TRUNK,0600);
-	int dst;
-	
-		if(!filename || fp == -1)
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
 		return (-1);
 
-		if(text_content)
-		{
-			dst = write(fd, text_content, _strlen(text_content));
-			if(dst == -1)
-			{
-				fclose(fp);
-				return (-1);
-			}
-		}
-	fclose(fp)
-return (1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-int _strlen(char *cont)
+int _strlen(char *cont);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
