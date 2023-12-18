884d1055915bf24c8d180b22b4280315daf134de
YASSINE LAHSSINI <slawiyassin152@gmail.com>
2023-12-18 14:36:07 +0100

    alx

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,30 @@
-#include "main.h"
-/**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
  */
-
-int _strlen(char * cont )
-{
-	int i = 0 ;
-
-	if(cont == NULL)
-	return (0);
-	while(cont[i])
-	i++;
-	return (i);
-}
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int *fp = open(filenmae,O_WRONLY  | O_APPEND);
-	ssize_t yassin;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if(fp == NULL || fp == -1)
-	 return (-1);
-	 if(text_content != NULL)
-	 {
-		yassin = write(fd, text_content, _strlen(text_content));
-		if(yassin == -1)
-		{
-			fclose(fp);
-			return(-1);
-		}
-	 }
-fclose(fp);
-return (1);
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
 }
